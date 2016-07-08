90e8c9b9cc41ef17cabe6efdf2b4b3a1d12def69
scriptype <scriptyper@gmail.com>
2016-07-08 14:38:11 +0300

    Improve styling and functionality; Add search

--- app.js
+++ app.js
@@ -11,12 +11,30 @@ window.addEventListener('DOMContentLoaded', function() {
   function hide(element) {
     element.style.cssText = 'transition: none; opacity: 0;'
   }
-  
+
   function fadeIn(element) {
     element.classList.add('fade-in')
     setTimeout(function() {
       element.classList.remove('fade-in')
     }, 300)
+  }
+
+  function _fetchAndPushState(url) {
+    var contents = document.querySelector('.main-contents')
+    var pagination = document.querySelector('.main-pagination')
+    var paginationButtons = document.querySelectorAll('#prev, #next')
+
+    fetch(url)
+      .then(function(result) { return result.text() })
+      .then(function(text) {
+        var fragment = DOMFragmentFromText(text)
+        pagination.outerHTML = fragment.querySelector('.main-pagination').outerHTML
+        hide(contents)
+        contents.outerHTML = fragment.querySelector('.main-contents').outerHTML
+        fadeIn(document.querySelector('.main-contents'))
+        history.pushState({}, '', url)
+        attachPaginationHandlers()
+      })
   }
 
   function attachPaginationHandlers() {
@@ -25,28 +43,44 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     var paginationButtons = document.querySelectorAll('#prev, #next')
-    var contents = document.querySelector('.main-contents')
-    var pagination = document.querySelector('.main-pagination')
 
     ;[].slice.call(paginationButtons).forEach(function(btn) {
       btn.addEventListener('click', function(event) {
         event.preventDefault()
-        fetch(btn.href)
-          .then(function(result) { return result.text() })
-          .then(function(text) {
-            var fragment = DOMFragmentFromText(text)
-            pagination.outerHTML = fragment.querySelector('.main-pagination').outerHTML
-            hide(contents)
-            contents.outerHTML = fragment.querySelector('.main-contents').outerHTML
-            fadeIn(document.querySelector('.main-contents'))
-            history.pushState({}, '', btn.href)
-            attachPaginationHandlers()
-          })
+        _fetchAndPushState(btn.href)
       })
     })
+  }
 
+  function _search(event) {
+    var value = event.which || event.keyCode
+    var notChanged = (
+        value === 36 || value === 37 || value === 38 || value === 39 ||
+        value === 13 || value === 16 || value === 27 || value === 20 ||
+        event.ctrlKey || event.shiftKey || event.metaKey
+    )
+    if (!notChanged) {
+      _fetchAndPushState('search?q=' + event.target.value)
+    }
+  }
+
+  function attachSearchHandlers() {
+    if (!window.history || !window.fetch) {
+      return
+    }
+
+    var searchForm = document.getElementById('search')
+    var searchInput = document.getElementById('q')
+    var debouncedSearch = _.debounce(_search, 300)
+
+    searchForm.addEventListener('submit', function (event) {
+      event.preventDefault()
+    })
+
+    searchInput.addEventListener('keydown', debouncedSearch)
   }
 
   attachPaginationHandlers()
+  attachSearchHandlers()
 
 })
